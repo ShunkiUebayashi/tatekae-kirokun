22bb13657c205d378925b843195eb68ef90cc878
ShunkiUebayashi <shunki0343@gmail.com>
2024-07-24 12:16:38 +0900

    add index

--- billing/models.py
+++ billing/models.py
@@ -8,13 +8,25 @@ class CustomUser(AbstractUser):
     secret_question = models.CharField(max_length=255)
     secret_answer = models.CharField(max_length=255)
 
+    class Meta:
+        indexes = [
+            models.Index(fields=["email"]),
+        ]
+
 
 class PendingInvoice(models.Model):
     sender = models.ForeignKey(CustomUser, related_name="sent_pending_invoices", on_delete=models.CASCADE)
     receiver = models.ForeignKey(CustomUser, related_name="received_pending_invoices", on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    date_created = models.DateTimeField(auto_now_add=True)  # フィールドを追加
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["sender"]),
+            models.Index(fields=["receiver"]),
+            models.Index(fields=["date_created"]),
+        ]
 
 
 class ApprovedInvoice(models.Model):
@@ -24,9 +36,16 @@ class ApprovedInvoice(models.Model):
     receiver = models.ForeignKey(
         settings.AUTH_USER_MODEL, related_name="received_approved_invoices", on_delete=models.CASCADE
     )
-    amount = models.PositiveIntegerField()  # PositiveIntegerFieldで1以上の整数のみを許可
+    amount = models.PositiveIntegerField()
     description = models.TextField()
-    date_created = models.DateTimeField(auto_now_add=True)  # フィールドを追加
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Invoice from {self.sender} to {self.receiver} - Amount: {self.amount}"
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["sender"]),
+            models.Index(fields=["receiver"]),
+            models.Index(fields=["date_created"]),
+        ]
